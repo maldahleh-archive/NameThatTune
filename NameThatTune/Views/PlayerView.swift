d27a603a2e9504b0b1fedcec85b25c2634b782b2
Mohammed Al-Dahleh <maldahleh@gmail.com>
2017-12-15 04:03:43 -0500

    Cleanup project structure

---
+++ NameThatTune/Views/PlayerView.swift
@@ -0,0 +1,21 @@
+//
+//  PlayerView.swift
+//  NameThatTune
+//
+//  Created by Mohammed Al-Dahleh on 2017-12-15.
+//  Copyright © 2017 Mohammed Al-Dahleh. All rights reserved.
+//
+
+import UIKit
+
+class PlayerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
